a0f0ed4d74ba1f46083820b657d9f054047efd82
AMANJAIN13 <42490630+AMANJAIN13@users.noreply.github.com>
2020-05-05 13:40:33 +0530

    Update provider.tf

--- audit/provider.tf
+++ audit/provider.tf
@@ -8,11 +8,11 @@
 */
 
 provider "aws" {
-  access_key = "AKIAR5RMWYRJI7WHJ6P2"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
   #region    = "ap-southeast-1"
   version = "~> v2.58.0"
 }
 
-#terraform init --backend-config="access_key=AKIAR5RMWYRJI7WHJ6P2" --backend-config="secret_key=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"
+#terraform init --backend-config="access_key=" --backend-config="secret_key="
